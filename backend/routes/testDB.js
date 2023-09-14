17638112d890621ca6bfb11b0040f8c6ea1d4e3f
Luiz Felipe <luizfelipesoma@gmail.com>
2023-09-14 08:02:24 -0300

    Começando resolucao do token

--- backend/routes/testDB.js
+++ backend/routes/testDB.js
@@ -2,38 +2,16 @@ var connection = require("../db");
 var express = require('express');
 var router = express.Router();
 
-// require("dotenv").config();
+router.get("/", (req, res) => {
+  connection.query('SELECT email FROM users;', (error, results) => {
+    if (error) {
+      res.status(500).json({ error: error }); // Enviar um status de erro em caso de falha na consulta
+    } else {
+      const emails = results.map(result => result.email.replace(/"/g, ''));
+      const concatenatedEmails = emails.join(', ');
+      res.send(concatenatedEmails);
+    }
+  });
+})
 
-// const jwt = require("jsonwebtoken");
-
-router.get('/', function (req, res) {
-    connection.connect();
-
-    connection.query("SELECT * FROM users", (error, result) => {
-      if(error){
-        return res.status(500).json({ message: "erro" });
-      }
-      return res.json({ message: result });
-    });
-
-    // connection.query('SELECT email FROM users;', (error, results) => {
-    //     if (error) {
-    //         res.status(500).json({ error: error }); // Enviar um status de erro em caso de falha na consulta
-    //     } else {
-    //         const emails = results.map(result => result.email.replace(/"/g, ''));
-    //         const concatenatedEmails = emails.join(', ');
-    //         res.send(concatenatedEmails);
-    //     }
-    // });
-});
-
-// router.get("/", (req, res) => {
-//     const email = "luiz@gmail.com";
-//     const token = jwt.sign({ email }, process.env.SECRET, {
-//         expiresIn: 300,
-//       })
-//       res.send(token);
-
-// });
- 
 module.exports = router;
