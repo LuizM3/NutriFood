c0c687832b8e3b89a218b8ea4ab6bf4855569e8b
bryantoken <bryanzborges1@gmail.com>
2023-11-02 20:59:32 -0300

    servidor funcionando😊

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-  // host: process.env.HOST,
-  // user: process.env.USER,
-  // password: process.env.PASSWORD,
-  // database: process.env.DATABASE,
-  host     : "localhost",
-  user     : "root",
-  password : "",
-  database : "nutrifood"
+const connection = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  // host     : "localhost",
+  // user     : "root",
+  // password : "",
+  // database : "nutrifood"
 });
 module.exports = connection;
