ba9b4f5248ce54e5297f4602501bc7867f80b202
AlexBrandao10 <alexsilvabrandao11@gmail.com>
2023-12-05 14:59:57 -0300

    Cypress Completo

--- frontend/cypress/e2e/cadastro.cy.js
+++ frontend/cypress/e2e/cadastro.cy.js
@@ -63,7 +63,7 @@ describe('signin page', () => {
     cy.get('#inline-checkbox-7').click();
     cy.get("#inline-radio-9").click();
     cy.getByData("links").find("button").contains('Enviar').click();
-    
+
     //Realiza o cadastro errado;
     cy.wait(2000);
     cy.getByData("form-email").type(randomEmail + "@gmail.com");
@@ -80,15 +80,62 @@ describe('signin page', () => {
     //Ajustes e Logout
     cy.get('#dropdown-autoclose-true').click();
     cy.get('.dropdown-menu > :nth-child(1)').click();
-    
+
+    //Novo Email
+    cy.get('.formulario-card > :nth-child(1)').type(randomEmail + "123Novo@gmail.com");
+    cy.get('.justify-content-end > div > .bt-sub').click();
+    cy.get('[data-test="form-pass"]').type("TesteCy123@");
+    cy.get('#button-login-signup').click();
+    cy.wait(1000);
+    cy.getByData("links").find("a").contains('Entrar').click();
+    cy.getByData("form-email").type(randomEmail + "123Novo@gmail.com");
+    cy.getByData("form-pass").type("TesteCy123@");
+    cy.get("#button-login-signup").click();
+    cy.wait(2000);
+
+    //Mudanças nos valores switch
+    cy.get('#dropdown-autoclose-true').click();
+    cy.get('.dropdown-menu > :nth-child(1)').click();
+    cy.get(':nth-child(2) > #custom-switch').click();
+    cy.get(':nth-child(4) > #custom-switch').click();
+    cy.get(':nth-child(5) > #custom-switch').click();
+    cy.get('#vinculo-radio-2').click();
+    cy.get('.div-pattern > [type="button"]').click();
+    cy.reload();
+    //Nova Senha
+    cy.get(':nth-child(2) > a > .bg-transparent').click();
+    cy.get(':nth-child(2) > [data-test="form-pass"]').type("TesteCy123@");
+    cy.get(':nth-child(4) > [data-test="form-pass"]').type("TesteCypress123@");
+    cy.get(':nth-child(5) > [data-test="form-pass"]').type("TesteCypress123@");
+    cy.get('#button-login-signup').click();
+    cy.wait(1000);
+    cy.getByData("links").find("a").contains('Entrar').click();
+    cy.getByData("form-email").type(randomEmail + "123Novo@gmail.com");
+    cy.getByData("form-pass").type("TesteCypress123@");
+    cy.get("#button-login-signup").click();
+    cy.wait(2000);
 
     //Sugestões
+    cy.visit("http://localhost:3000");
     cy.getByData("links").find("a").contains('Sugestões').click();
     cy.get('.bt-sub').click();
     cy.reload();
     cy.get('.form-control').type("Excelente espaço");
     cy.get('.bt-sub').click();
     cy.wait(1000);
+
+    //Alterando e excluindo sugestão
+    cy.get('#dropdown-autoclose-true').click();
+    cy.get('.dropdown-menu > :nth-child(1)').click();
+    cy.get(':nth-child(3) > a > .bg-transparent > .align-items-center').click();
+    cy.get('.btn-pencil').click();
+    cy.get('.form-control').type("Gostei do ambiente!");
+    cy.get('.d-flex > .h-100').click();
+    cy.get('.btn-trash').click(); //Clica duas vezes por erro
+    cy.get('.btn-trash').click();
+    cy.get('.bt-sub').click();
+    cy.wait(1000);
+    cy.get('.text-decoration-none').click();
 
     //Avaliação;
     cy.getByData("links").find("a").contains('Avaliações').click();
@@ -103,7 +150,7 @@ describe('signin page', () => {
     cy.get('[for="star5_group9"]').click();
     cy.get('[for="star5_group10"]').click();
     cy.get('.btn-reviews').click();
-    
+    cy.visit("http://localhost:3000");
   })
 
 })
